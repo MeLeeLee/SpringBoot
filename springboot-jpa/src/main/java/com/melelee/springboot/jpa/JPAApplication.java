1cda3481f8dc6a19d41a0171af4b6002977f3cc2
melelee <melelee@qq.com>
2018-07-11 15:11:32 +0800

    结构整理

---
+++ springboot-jpa/src/main/java/com/melelee/springboot/jpa/JPAApplication.java
@@ -0,0 +1,21 @@
+package com.melelee.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableScheduling
+public class JPAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPAApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate newRestTemplate(){
+		return new RestTemplate();
+	}
+}
